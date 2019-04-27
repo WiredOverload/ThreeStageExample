f1f362afb1a96f5d3cfc2e59c7089a226df7d52e
Michael Hodges <mike.everywhere@gmail.com>
2019-04-27 00:49:06 -0500

    Added TODO List

--- src/index.js
+++ src/index.js
@@ -1,8 +1,51 @@
 "use strict";
 /**
- * Notes:
+ * TODO For Library:
  * Buttons and other interactable elements need more work in stage
  * individual stage update methods defined here?
+ *
+ * Game Plan:
+ * Rogue Legacy, Now With Bees (TM)
+ * A bunch of bees dressed up as a man
+ * platformer inside a mansion(?)
+ *  No idea why he's there or what his end goal is
+ * originally hiding himself as a man to fit in with society, which uses bees as currency
+ * loses bee mass as he gets hit due to losing bees
+ * loses bee mass as he uses Bee Abilities (TM)
+ * leaves the queen when the swarm disperses
+ *  pickup previosuly dropped queens used for upgrades at shop
+ *
+ *
+ * TODO For Game
+ * Splash stage
+ * lose stage
+ * shop stage
+ * player movement + properties
+ * projectiles
+ * enemies
+ *  melee + ranged?
+ *  settled on wasps + exterminators? what about society?
+ * platforms and environment
+ *  mansion just for platform stage?
+ *  possibly trying to escape a city?
+ *  too many bees flew in through windows and banded together to escape?
+ * art
+ *  player, multiple sprites or dynamic
+ *  platforms
+ *  projectiles
+ *  enemies
+ *  backgrounds
+ *  other stage screens
+ * sound
+ *  music
+ *  bee sounds? we need it but it could sound traumatizing
+ * different playyer attacks
+ *  keep it simple by just spawning bee projectiles at first
+ *  upgrades could make bees homing, bounce, be reobtainable...
+ * Score, time or number of times died
+ * NPCs that attack you if it's too obvious you are bees?
+ *
+ * Feel free to add anything you want here and delete anything that's been completed
  */
 exports.__esModule = true;
 var three_1 = require("three");
