6952f7ee122b41b97ba231a03fbaea5b42e6e98b
Michael Hodges <mike.everywhere@gmail.com>
2019-04-27 16:28:14 -0500

    added projectile

---
+++ src/projectile.js
@@ -0,0 +1,38 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
+var three_1 = require("three");
+var stage_1 = require("./stage");
+var THREE = require('three'); //only needed due to three type shenanigans
+var Projectile = /** @class */ (function (_super) {
+    __extends(Projectile, _super);
+    function Projectile(scene, x, y, imageURL) {
+        var _this = _super.call(this) || this;
+        _this.x = x;
+        _this.y = y;
+        var spriteMap = new THREE.TextureLoader().load(imageURL); //"BoundingBox.png"
+        var spriteMaterial = new THREE.SpriteMaterial({ map: spriteMap, color: 0xffffff });
+        _this.sprite = new three_1.Sprite(spriteMaterial);
+        scene.add(_this.sprite);
+        return _this;
+    }
+    Projectile.prototype.render = function () {
+    };
+    Projectile.prototype.update = function () {
+        //no need to update a static Image
+    };
+    return Projectile;
+}(stage_1.Updateable));
+exports.Projectile = Projectile;
